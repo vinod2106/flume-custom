4e38531850865f6783ffe7c6ae057af50dbd82b4
vinod2106 <vinod254581@gmail.com>
2017-10-27 16:36:36 +0200

    Made changes in the testcase

--- src/test/java/com/shavinod/flume/source/TestHazelcastQueueClient.java
+++ src/test/java/com/shavinod/flume/source/TestHazelcastQueueClient.java
@@ -1,4 +1,5 @@
 package com.shavinod.flume.source;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,44 +7,43 @@ import org.slf4j.LoggerFactory;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.concurrent.BlockingQueue;
+
 
 public class TestHazelcastQueueClient {
 	public static final Logger logger = LoggerFactory.getLogger(TestHazelcastQueueClient.class);
 	private HazelcastInstance hazelcastClient;
 	private BlockingQueue<String> distributedQueue;
-	private InetAddress ip;
 
-	private void init(String queueName) throws UnknownHostException {
+	private void init(String queueName){
 		// TODO Auto-generated method stub
 		logger.info("queueName =======> " + queueName);
-		ip = InetAddress.getLocalHost();
-		logger.info("ip =======> " + ip);
-		String hostname = ip.getHostName();
-        logger.info("hostname =======> " + hostname);
 		ClientConfig clientConfig = new ClientConfig();
 		clientConfig.getGroupConfig().setName("dev").setPassword("dev-pass");
-		clientConfig.getNetworkConfig().addAddress(hostname);
+		clientConfig.getNetworkConfig().addAddress("120.0.0.1");
 		hazelcastClient = HazelcastClient.newHazelcastClient(clientConfig);
 		distributedQueue = hazelcastClient.getQueue(queueName);
 	}
 
 	@Test
-	public void test() throws NumberFormatException, InterruptedException, UnknownHostException {
+	public void test() {
 		// Send the messages to queue
 		TestHazelcastQueueClient hazelcastQueueClient = new TestHazelcastQueueClient();
 		hazelcastQueueClient.init("queue");
 		hazelcastQueueClient.sendTextMessages(Integer.parseInt("5"));
 	}
 
-	private void sendTextMessages(int numOfTimes) throws InterruptedException {
+	private void sendTextMessages(int numOfTimes) {
 		// TODO Auto-generated method stub
 
 		for (int i = 0; i < numOfTimes; i++) {
-			distributedQueue.add("msg#" + i);
+			try {
+				distributedQueue.add("msg#" + i);
+			} catch (Exception e) {
+				// TODO: handle exception
+				logger.error("Error in the send msg method " + e);
+				e.printStackTrace();
+			}
 
 		}
 
